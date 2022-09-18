16c9672ddfffcc27aadf88a081c0120c65015405
Kunal Maurya <krsm1470@gmail.com>
2022-09-18 18:40:49 +0530

    dp

---
+++ Dynamic Programming/Trapping_Rain_Water.java
@@ -0,0 +1,19 @@
+ class Solution {
+    public int trap(int[] height) {
+        if (height==null) return 0;
+        Stack<Integer> s = new Stack<Integer>();
+        int i = 0, maxWater = 0, maxBotWater = 0;
+        while (i < height.length){
+            if (s.isEmpty() || height[i]<=height[s.peek()]){
+                s.push(i++);
+            }
+            else {
+                int bot = s.pop();
+                maxBotWater = s.isEmpty()? // empty means no il
+                0:(Math.min(height[s.peek()],height[i])-height[bot])*(i-s.peek()-1);
+                maxWater += maxBotWater;
+            }
+        }
+        return maxWater;
+    }
+}
