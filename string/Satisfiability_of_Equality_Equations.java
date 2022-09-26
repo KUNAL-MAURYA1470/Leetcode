65201b31220192e5cf02a35924e073190f144ad4
Kunal Maurya <krsm1470@gmail.com>
2022-09-26 20:15:52 +0530

    added

---
+++ string/Satisfiability_of_Equality_Equations.java
@@ -0,0 +1,28 @@
+class Solution {
+    int[] parent = new int[26];
+    public int find(int x) {
+     if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+        
+    }
+    public boolean equationsPossible(String[] equations) {
+         int n = equations.length;
+         for (int i = 0; i < 26; i++) parent[i] = i;
+         for (String e : equations) {
+            
+            if (e.charAt(1) == '=') {
+                
+                parent[find(e.charAt(0)- 'a')] = find(e.charAt(3) - 'a');
+            }
+        }
+        for (String e : equations) {
+           
+            if (e.charAt(1) == '!' && find(e.charAt(0) - 'a') == find(e.charAt(3) - 'a')) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+
+}
